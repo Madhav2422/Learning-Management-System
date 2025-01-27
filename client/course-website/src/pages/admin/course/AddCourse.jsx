4b6c22264b27f128ecc0994e2e40f805deda1dc9
Madhav Hirani <madhavhirani2422@gmail.com>
2025-01-27 16:57:28 +0530

    mern

---
+++ client/course-website/src/pages/admin/course/AddCourse.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddCourse = () => {
+    return (
+        <div className='flex-1 mx-10'>
+            <div className='mb-4'>
+                <h1 className='font-bold  text-4xl'>Lets add course,add some basic details for your new course</h1>
+                <p className='text-sm'>
+                    Provide the essential details to create and customize your new course effortlessly
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AddCourse
