20d3db2a301d00979d47b66ba7eeeb46ae1fed7f
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-01 12:29:57 +0530

    mern:

--- client/course-website/src/pages/admin/course/AddCourse.jsx
+++ client/course-website/src/pages/admin/course/AddCourse.jsx
@@ -1,5 +1,6 @@
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from "@/components/ui/input";
 import {
     Select,
     SelectContent,
@@ -7,51 +8,96 @@ import {
     SelectLabel,
     SelectTrigger,
     SelectValue,
-  } from "@/components/ui/select"
+} from "@/components/ui/select"
 import { SelectGroup } from '@radix-ui/react-select'
 import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
-  
+import { Loader2 } from 'lucide-react'
+
 const AddCourse = () => {
 
-    const navigate=useNavigate()
+    const [courseTitle, setCourseTitle] = useState("");
+    const [category, setCategory] = useState("");
+
+    const navigate = useNavigate()
+    const isLoading = false;
+
+    const createCourseHandler = async () => {
+        console.log(courseTitle, category);
+    }
+
+    const getSelectedCategory = (value) => {
+        setCategory(value)
+    }
 
     return (
-        <div className='flex-1 mx-10'>
-            <div className='mb-4'>
-                <h1 className='font-bold  text-xl'>Lets add course,add some basic details for your new course</h1>
-                <p className='text-sm'>
-                    Provide the essential details to create and customize your new course effortlessly
+        <div className="flex-1 mx-10">
+            <div className="mb-4">
+                <h1 className="font-bold text-xl">
+                    Lets add course, add some basic course details for your new course
+                </h1>
+                <p className="text-sm">
+                    Provide essential details to create your new course. Choose a relevant title and category to help students easily find and enroll in your course.
                 </p>
             </div>
-            <div className='space-y-4'>
-                <Label>Title</Label>
-                <input type="text" name="courseTitle" placeholder="Your Course Name" />
+            <div className="space-y-4">
+                <div>
+                    <Label>Title</Label>
+                    <Input
+                        type="text"
+                        value={courseTitle}
+                        onChange={(e) => setCourseTitle(e.target.value)}
+                        placeholder="Your Course Name"
+                    />
+                </div>
+                <div>
+                    <Label>Category</Label>
+                    <Select onValueChange={getSelectedCategory}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Select a category" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Category</SelectLabel>
+                                <SelectItem value="Next JS">Next JS</SelectItem>
+                                <SelectItem value="Data Science">Data Science</SelectItem>
+                                <SelectItem value="Frontend Development">
+                                    Frontend Development
+                                </SelectItem>
+                                <SelectItem value="Fullstack Development">
+                                    Fullstack Development
+                                </SelectItem>
+                                <SelectItem value="MERN Stack Development">
+                                    MERN Stack Development
+                                </SelectItem>
+                                <SelectItem value="Javascript">Javascript</SelectItem>
+                                <SelectItem value="Python">Python</SelectItem>
+                                <SelectItem value="Docker">Docker</SelectItem>
+                                <SelectItem value="MongoDB">MongoDB</SelectItem>
+                                <SelectItem value="HTML">HTML</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Button variant="outline" onClick={() => navigate("/admin/course")}>
+                        Back
+                    </Button>
+                    <Button disabled={isLoading} onClick={createCourseHandler}>
+                        {isLoading ? (
+                            <>
+                                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                Please wait
+                            </>
+                        ) : (
+                            "Create"
+                        )}
+                    </Button>
+                </div>
             </div>
-            <div className='space-y-4'>
-                <Label>Category</Label>
-                <Select>
-                    <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Select a category" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>Category</SelectLabel>
-                            <SelectItem value="apple">Apple</SelectItem>
-                            <SelectItem value="banana">Banana</SelectItem>
-                            <SelectItem value="blueberry">Blueberry</SelectItem>
-                            <SelectItem value="grapes">Grapes</SelectItem>
-                            <SelectItem value="pineapple">Pineapple</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <Button variant="outline" onClick={()=>navigate("/admin/course")}>Back</Button>
-                    <Button>Create</Button>
-                </div>
         </div>
-    )
-}
+    );
+};
+
 
 export default AddCourse
